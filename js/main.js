5444d2dfbf8dc854d97eef132535b04b556a5920
Farzod <107624021+farzod-achilov@users.noreply.github.com>
2022-08-20 10:26:31 +0500

    fixed

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var elInput = document.querySelector("[data-input]");
+var elBtn = document.querySelector("[data-btn]");
+var elList = document.querySelector("[data-list]");
+
+elBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+
+  var elLi = document.createElement("li");
+  elLi.textContent = elInput.value;
+
+  elList.appendChild(elLi);
+});
